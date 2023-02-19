604452c5b8a6446471661fa13b5263566ab74530
kranthiganapathiraju <53516229+kranthiganapathiraju@users.noreply.github.com>
2023-02-18 19:45:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+// import ReactDOM from "react-dom";
+// import {Routes,Route, Link } from "react-router-dom";
+
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" home="Welcome" />
+      {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
